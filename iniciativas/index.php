b94903e75f8ac45c7e1db379a47d34491eafdfdc
Carlos Hugo Gonzalez (Fundar) <carlos@fundar.org.mx>
2014-08-29 19:18:58 -0500

    iniciativas - ya se obtienen los titulos de las inicitiavas

--- iniciativas/index.php
+++ iniciativas/index.php
@@ -1,100 +1,66 @@
 <?php
 $ch = curl_init();
 
+#Curl a la primera parte de las iniciativas legislatura 62
 curl_setopt($ch, CURLOPT_URL, "http://gaceta.diputados.gob.mx/Gaceta/Iniciativas/62/gp62_a2primero.html");
 curl_setopt($ch, CURLOPT_TIMEOUT, 30);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
-$resultados  = curl_exec($ch);
-$explode     = explode('<font color="#CC0000">', $resultados);
+#obtenemos el resultado y quitamos script tags menos las permitidas, quitamos head-style, solo permite p,a,li,ul,font y br
+$resultado = curl_exec($ch);
+$resultado = eregi_replace("<head[^>]*>.*</head>", "",     $resultado);
+$resultado = eregi_replace("<style[^>]*>.*</style>", "",   $resultado);
+$resultado = strip_tags($resultado, '<p><a><li><ul><font><br><br/>');
+
+#Hacemos el explode de este codigo que delimita las fechas de seeciones de las iniciativas con titulos rojos
+$explode     = explode('<font color="#CC0000">', $resultado);
 $iniciativas = array();
 
-if(is_array($explode) and count($explode) > 0) {
+#si exsite un array y es mayor a 1 si no "algo anda mal" by pacojaso! y  eliminamos la primiera posicion no nos sirve porque es el header del html
+if(is_array($explode) and count($explode) > 1) {
 	unset($explode[0]);
 
+	#recorremos el array
 	foreach($explode as $value) {
+		//~ 
+		#obtenemos la fecha en que se publico la inicitava haciendo un explode de font donde termina el titulo rojo
 		$fecha_array   = explode('</font>', $value);
+		
+		#declaramos arrays que ocuparemos adelante
 		$iniciativa    = array();
 		$data          = array();
 		$data["fecha"] = "";
 		
+		#si las fecha existe la guardamos en array
 		if(is_array($fecha_array) and count($fecha_array) > 0) {
 			$data["fecha"] = $fecha_array[0];
 		}
 		
+		#despues viena la lista de inicativas por bloque en ul>li
 		$listas = explode('<ul>', $value);
 		
+		#comprobamos que exista al menos 1 y eliminamos la prima posicion que es basura de html
 		if(is_array($listas) and count($listas) > 0) {
 			unset($listas[0]);
+			
+			#recorremos el array de la lista de iniciativas y hacemos un explode para determinar el inicio y fin /ul li
 			foreach($listas as $lista) {
 				$elementos = explode('</ul>', $lista);
 				$elementos = explode('<li>', $elementos[0]);
 				
+				#si no es nulo el elemento
 				if(!is_null($elementos[1])) {
-					var_dump($elementos[1]);
+					#obtenes el titulo separado por salto de linea
+					$titulo_array = explode('<br>', $elementos[1]);
+					$titulo_array = explode('</br>', $titulo_array[0]);
+					
+					#titulos
+					echo trim($titulo_array[0]) . "<br/>";
 				}
 			}
 		}
 	}
 } else {
 	die("Algo extraño ocurrio :/");
-}
-
-die("fin");
-
-foreach($ids as $id) {
-	curl_setopt($ch, CURLOPT_URL, "http://sil.gobernacion.gob.mx/Librerias/pp_PerfilLegislador.php?SID=&Referencia=" . $id);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-
-	$resultado = curl_exec($ch);
-	
-	$explode = explode("<td class='tddatosazul'>Senador Propietario:<br><b>", $resultado);
-	
-	if(isset($explode[1])) {
-		$explode = explode('<br>', $explode[1]);
-		$nombre  = $explode[0];
-	} else {
-		$explode = explode("<td class='tddatosazul'>Senadora Propietario:<br><b>", $resultado);
-		if(isset($explode[1])) {
-			$explode = explode('<br>', $explode[1]);
-			$nombre  = $explode[0];
-		} else {
-			$nombre = "null";
-		}
-	}
-
-	
-	$explode = explode("<td class='tdcriterio'>&nbsp;Partido:</td>", $resultado);
-	if(isset($explode[1])) {
-		$explode = explode("<td class='tddatosazul'>", $explode[1]);
-		$explode = explode("</td>", $explode[1]);
-		$partido = $explode[0];
-	} else {
-		$partido = "null";
-	}
-
-
-	$explode = explode('nico:</td>', $resultado);
-	if(isset($explode[1])) {
-		$explode = explode("<td class='tddatosazul'>", $explode[1]);
-		$explode = explode("</td>", $explode[1]);
-		$email   = $explode[0];
-	} else {
-		$email = "null";
-	}
-
-
-	$explode = explode("fono:</td>", $resultado);
-	if(isset($explode[1])) {
-		$explode = explode("<td class='tddatosazul'>", $explode[1]);
-		$explode = explode("</td>", $explode[1]);
-		$telefono = $explode[0];
-	} else {
-		$telefono = "null";
-	}
-
-	echo $nombre . "|" . $partido . "|" . $email . "|" . $telefono . "<br/>";
 }
