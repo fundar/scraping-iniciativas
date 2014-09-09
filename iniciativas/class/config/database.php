ac29954bcb8e4b2aa87e49b8fe9ecc86164e1c44
Instancia Curul501 <carlos@fundar.org.mx>
2014-09-09 22:24:23 +0000

    database config

--- iniciativas/class/config/database.php
+++ iniciativas/class/config/database.php
@@ -3,6 +3,6 @@
 //Config database
 $db["dbHost"] = "localhost";
 $db["dbPort"] = "3306";
-$db["dbName"] = "iniciativas_scrapper";
+$db["dbName"] = "curul501";
 $db["dbUser"] = "root";
-$db["dbPwd"]  = "root";
+$db["dbPwd"]  = "Um8UJjfJEy1CFayg2ZBM";
