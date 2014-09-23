f264f2af7d6b7aeb5328fda2d3df8be8fc4c6397
Carlos Hugo Gonzalez (Fundar) <carlos@fundar.org.mx>
2014-09-23 18:43:29 -0500

    config de arrays de periodos

---
+++ iniciativas/class/config/array_periodos.php
@@ -0,0 +1,79 @@
+<?php
+
+#array de periodos
+$array_periodos = array();
+
+/*Primer año*/
+$array_periodos[] = array(
+	"periodo" => 'Primer periodo ordinario',
+	"url"     => "/Gaceta/Iniciativas/62/gp62_a1primero.html",
+	"ano"     => "Primer año"
+);
+
+$array_periodos[] = array(
+	"periodo" => '1er. periodo Com. Permanente',
+	"url"     => "/Gaceta/Iniciativas/62/gp62_a1perma1.html",
+	"ano"     => "Primer año"
+);
+
+$array_periodos[] = array(
+	"periodo" => 'Segundo periodo ordinario',
+	"url"     => "/Gaceta/Iniciativas/62/gp62_a1segundo.html",
+	"ano"     => "Primer año"
+);
+
+$array_periodos[] = array(
+	"periodo" => 'Primer periodo ordinario',
+	"url"     => "/Gaceta/Iniciativas/62/gp62_a1perma2.html",
+	"ano"     => "2o. periodo Com. Permanente "
+);
+
+/*Segundo año*/
+$array_periodos[] = array(
+	"periodo" => 'Primer periodo ordinario',
+	"url"     => "/Gaceta/Iniciativas/62/gp62_a2primero.html",
+	"ano"     => "Segundo año"
+);
+
+$array_periodos[] = array(
+	"periodo" => '1er. periodo Com. Permanente',
+	"url"     => "/Gaceta/Iniciativas/62/gp62_a2perma1.html",
+	"ano"     => "Segundo año"
+);
+
+$array_periodos[] = array(
+	"periodo" => 'Segundo periodo ordinario',
+	"url"     => "/Gaceta/Iniciativas/62/gp62_a2segundo.html",
+	"ano"     => "Segundo año"
+);
+
+$array_periodos[] = array(
+	"periodo" => '2o. periodo Com. Permanente',
+	"url"     => "/Gaceta/Iniciativas/62/gp62_a2perma2.html",
+	"ano"     => "Segundo año"
+);
+
+$array_periodos[] = array(
+	"periodo" => 'Primer periodo extraordinario',
+	"url"     => "/Gaceta/Iniciativas/62/gp62_a2extra1.html",
+	"ano"     => "Segundo año"
+);
+
+$array_periodos[] = array(
+	"periodo" => 'Segundo periodo extraordinario',
+	"url"     => "/Gaceta/Iniciativas/62/gp62_a2extra2.html",
+	"ano"     => "Segundo año"
+);
+
+$array_periodos[] = array(
+	"periodo" => 'Cuarto periodo extraordinario',
+	"url"     => "/Gaceta/Iniciativas/62/gp62_a2extra4.html",
+	"ano"     => "Segundo año"
+);
+
+/*Tercer año*/
+$array_periodos[] = array(
+	"periodo" => 'Primer periodo ordinario',
+	"url"     => "/Gaceta/Iniciativas/62/gp62_a3primero.html",
+	"ano"     => "Tercer año"
+);
