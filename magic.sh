e68c466ca17befd83caffc0263b1b85b8079948f
Carlos Hugo Gonzalez (Fundar) <carlos@fundar.org.mx>
2014-10-22 15:58:07 -0500

    Magic.sh

--- magic.sh
+++ magic.sh
@@ -2,4 +2,4 @@ NOWF=$(date +"%F")
 NOWT=$(date +"%T")
  
 php iniciativas/index.php > iniciativas/logs/iniciativas-$NOWF-$NOWT.log &
-tail -f iniciativas/logs/iniciativas-$NOWF-$NOWT.log
+#tail -f iniciativas/logs/iniciativas-$NOWF-$NOWT.log
